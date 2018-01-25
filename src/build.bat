beb2c80931460a52bf557e3813ebdcae15cfa02d
Moses_of_Egypt <Moses_of_Egypt@localhost>
2018-01-24 22:47:31 -0500

    Added license and installer builder script for Inno Setup 5 Fixed the build.bat deleting the root .hg folder

--- src/build.bat
+++ src/build.bat
@@ -1,5 +1,4 @@
 python "%~dp0build_meke.py" build -b "%~dp0.."
 
 echo Cleaning extraneous files and folders...
-python "%~dp0exe_lib_cleaner.py" "%~dp0.."
-pause
+python "%~dp0exe_lib_cleaner.py" "%~dp0..\exe.win-amd64-3.5"
