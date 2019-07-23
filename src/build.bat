02fbf722401e34ea946524bacd9a8e76a3438c5e
Moses_of_Egypt <mosesbobadilla@gmail.com>
2019-07-23 12:47:47 -0400

    Added call to compile setup file at end of build script

--- src/build.bat
+++ src/build.bat
@@ -2,3 +2,4 @@ python "%~dp0build_meke.py" build -b "%~dp0.."
 
 echo Cleaning extraneous files and folders...
 python "%~dp0exe_lib_cleaner.py" "%~dp0..\exe.win-amd64-3.5"
+"C:\Program Files (x86)\Inno Setup 5\iscc.exe" "%~dp0make_installer.iss"
