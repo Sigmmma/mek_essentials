db3dd1d2bb0e1719645ce2ff477781237e290fe8
gbMichelle <gbmichelle.dev@gmail.com>
2020-03-07 08:43:16 +0100

    I think I want to squash these (WIP)

--- build_runners.py
+++ build_runners.py
@@ -11,6 +11,7 @@ parser = ArgumentParser(description='Build PyInstaller Exes')
 parser.add_argument('--py-exe', help='The python exe to use. USE PYTHON 3.6', required=True)
 cmd_args = parser.parse_args()
 
+BUILD_DIR        = path.join(os.getcwd(), "build")
 RUNNERS_DIR      = path.join(os.getcwd(), "runners")
 RUNNERS_DIST_DIR = path.join(RUNNERS_DIR, "dist")
 ICONS_DIR        = path.join(os.getcwd(), "icons")
@@ -35,7 +36,7 @@ for program in MEK_PROGRAMS:
 
 # Copy the runners into the main directory.
 runner_names = filter(lambda n : n.lower().endswith(".exe"),
-                    os.listdir(RUNNERS_DIR))
+                    os.listdir(RUNNERS_DIST_DIR))
 
 for name in runner_names:
-    shutil.copyfile(path.join(RUNNERS_DIR, name), path.join(BUILD_DIR, name))
+    shutil.copyfile(path.join(RUNNERS_DIST_DIR, name), path.join(BUILD_DIR, name))
